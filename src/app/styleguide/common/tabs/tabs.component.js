7f1565dbb0bc333bcbfebae9c49eeb8893cc6256
Gavyn McKenzie <gavyn@etchapps.com>
2016-10-10 12:58:48 +0100

    feat: Add tab control

--- src/app/styleguide/common/tabs/tabs.component.js
+++ src/app/styleguide/common/tabs/tabs.component.js
@@ -2,20 +2,29 @@ const TabsComponent = {
   bindings: {
     types: '<',
   },
+  controller: function () {
+    this.$onInit = () => {
+      this.selected = 0;
+    };
+
+    this.selectTab = (index) => {
+      this.selected = index;
+    };
+  },
   template: `
     <div class="sg-tabs">
 
       <ul class="sg-tabs__list" id="sg-tabs">
-        <li class="sg-tabs__item sg-tabs__item--active" ng-repeat="type in $ctrl.types">
-          <a href="#{{type.type}}" class="sg-tabs__button">
-            <i class="icon icon-{{type.type.toLowerCase()}}"></i>
+        <li class="sg-tabs__item ng-class:{ 'sg-tabs__item--active': $ctrl.selected == $index }" ng-repeat="type in $ctrl.types">
+          <a href="#{{type.type}}" class="sg-tabs__button" ng-click="$ctrl.selectTab($index)">
+            <i class="icon icon-{{type.type.toLowerCase()}} {{$ctrl.selected}} {{$index}}"></i>
             <span>{{type.type}}</span>
           </a>
         </li>
       </ul>
 
       <div class="sg-tabs__content">
-        <div class="sg-tabs__pane sg-tabs__pane--active" id="atoms" ng-repeat="type in $ctrl.types">
+        <div class="sg-tabs__pane ng-class:{ 'sg-tabs__pane--active': $ctrl.selected == $index }" id="atoms" ng-repeat="type in $ctrl.types">
           <sg-nav components=type.components></sg-nav>
         </div>
       </div>
