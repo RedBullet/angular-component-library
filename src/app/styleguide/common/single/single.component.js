c3f0482550e9751d09c3130adf0dbc258ab03f54
Simon Jones <simonj@redbullet.co.uk>
2016-10-11 10:23:17 +0100

    fix: show schema on single view

--- src/app/styleguide/common/single/single.component.js
+++ src/app/styleguide/common/single/single.component.js
@@ -6,7 +6,7 @@ const SingleComponent = {
   },
   controller: SingleController,
   template: `
-    <sg-component-detail name="{{$ctrl.component.name}}" docs="{{$ctrl.component.docs}}" type="{{$ctrl.type.type}}" properties=$ctrl.component.schema.properties></sg-component-detail>
+    <sg-component-detail name="{{$ctrl.component.name}}" docs="{{$ctrl.component.docs}}" type="{{$ctrl.type.type}}" schema=$ctrl.component.schema></sg-component-detail>
   `
 };
 
