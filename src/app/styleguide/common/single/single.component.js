1664a8e3cdaf86894c1cfdf71e8826ab95ed3365
Gavyn McKenzie <gavyn@etchapps.com>
2016-10-11 09:39:54 +0100

    fix: Single component schema

--- src/app/styleguide/common/single/single.component.js
+++ src/app/styleguide/common/single/single.component.js
@@ -3,7 +3,7 @@ import SingleController from './single.controller';
 const SingleComponent = {
   controller: SingleController,
   template: `
-    <sg-component-detail name="{{$ctrl.component.name}}" docs="{{$ctrl.component.docs}}" type="{{$ctrl.type.type}}" properties=$ctrl.component.schema.properties></sg-component-detail>
+    <sg-component-detail name="{{$ctrl.component.name}}" docs="{{$ctrl.component.docs}}" type="{{$ctrl.type.type}}" schema=$ctrl.component.schema></sg-component-detail>
   `
 };
 
