226cd3107459c43f229aa17a7cccbd9e11d66995
Gavyn McKenzie <gavyn@etchapps.com>
2017-01-25 16:46:26 +0000

    refactor: Hide properties table if we have no properties for this component

--- src/app/styleguide/common/properties/properties.component.js
+++ src/app/styleguide/common/properties/properties.component.js
@@ -1,10 +1,16 @@
+class PropertiesController {
+  $onInit() {
+    this.hasProperties = !angular.equals({}, this.properties);
+  }
+}
+
 const PropertiesComponent = {
   bindings: {
     properties: '<',
     required: '<',
   },
   template: `
-    <section class="sg-properties">
+    <section class="sg-properties" ng-if="$ctrl.hasProperties">
       <h3 class="sg-properties__title">Properties</h3>
       <table class="sg-properties__table">
         <thead>
@@ -18,7 +24,7 @@ const PropertiesComponent = {
           <td class="sg-properties__property__type">{{property.type}}</td>
           <td class="sg-properties__property__required" ng-class="{'sg-properties__property__required--optional': $ctrl.required.indexOf(key) < 0, 'sg-properties__property__required--required': $ctrl.required.indexOf(key) > -1}">
             <div ng-if="$ctrl.required.indexOf(key) < 0">
-              <i class="icon icon-tick"></i>
+              <i class="styleguide-icon styleguide-icon-tick"></i>
               <span>Optional</span>
             </div>
           </td>
@@ -26,7 +32,8 @@ const PropertiesComponent = {
         </tr>
       </table>
     </section>
-  `
+  `,
+  controller: PropertiesController,
 };
 
 export default PropertiesComponent;
